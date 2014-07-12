eb15d7649b453cfc885d4bc5828d193e3f8c631a
nicolas <fonsat.nicolas@gmail.com>
2014-07-12 03:06:33 +0200

    Ajout de la homepage qui va permettre de rechercher un projet. Ajout des liens javascript et css dans index.html. Ajout des traductions fr et en pour cette page. Ajout d'image GitHub.

--- js/controllers/HomepageController.js
+++ js/controllers/HomepageController.js
@@ -2,7 +2,24 @@
         
 var homepageController = [
     '$scope',
-    function ($scope){
-        $scope.name = 'Nicolas';
+    '$http',
+    function ($scope, $http){
+
+        var listerRepository = function () {
+            $http.get('https://api.github.com/repositories').
+                success(
+                    function (data) {
+                        $scope.listRepo = data;
+                        console.log($scope.listRepo)
+                    }
+                ).
+                error(
+                    function(data, status, headers, config) {
+                        console.log('error : ' + status)
+                    }
+                );
+        }
+
+        listerRepository();
     }
 ];
